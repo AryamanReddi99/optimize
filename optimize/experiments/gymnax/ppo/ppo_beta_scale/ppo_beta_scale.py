fee10b82f8e98b142c9aa6e7397627936272e049
AryamanReddi99 <aryamanreddi@gmail.com>
2025-08-13 20:30:14 +0200

    changes

--- optimize/experiments/gymnax/ppo/ppo_beta_scale/ppo_beta_scale.py
+++ optimize/experiments/gymnax/ppo/ppo_beta_scale/ppo_beta_scale.py
@@ -494,7 +494,8 @@ def make_train(config):
                 )
 
                 css_mu_prev = total_loss[1]["cosine_similarity_mu_prev"].mean()
-                beta_1_new = jnp.exp(-config["beta_exp_scale"] * css_mu_prev)
+                css = total_loss[1]["cosine_similarity"].mean()
+                beta_1_new = jnp.exp(-config["beta_exp_scale"] * css)
                 beta_1_new = jnp.clip(
                     beta_1_new, config["beta_1_min"], config["beta_1_max"]
                 )
