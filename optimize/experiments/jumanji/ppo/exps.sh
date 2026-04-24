7e6512b9f83d0952069701f0985279e1d939e9a7
AryamanReddi99 <aryamanreddi@gmail.com>
2026-04-24 12:15:38 +0200

    changes

--- optimize/experiments/jumanji/ppo/exps.sh
+++ optimize/experiments/jumanji/ppo/exps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 conda activate adam
-env_name=Connector-v3
+env_name=Sokoban-v0
 python ppo_discrete.py env_name=$env_name optimizer=adam
 sleep 10
 python ppo_discrete.py env_name=$env_name optimizer=myano myano_gamma=0.0 beta_2=0.92
